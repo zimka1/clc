c32f114f9172355052dab37e706889d46b4dc739
zimka <zimkail2015@mail.ru>
2021-03-14 14:22:38 +0300

    separation Added splitting numbers and converting them to Int

--- clc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio>
-#include <string>
-
-
-int main()
-{
-    printf("Hello world/n")
-}
--- clc.cpp
+++ clc.cpp
@@ -6,8 +6,22 @@ using namespace std;
 
 int main()
 {
-    string s;
-    cin >> s;
-    cout << s;
+    string s, s1, s2;
+    char sign;
+    getline(cin,s);
+    int n = s.size();
+    int fl = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (fl == 0) s1 += s[i];
+        if (fl == 1) sign = s[i];
+        if (fl == 3) s2 += s[i];
+        if (s[i] == ' ' || s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') fl++;
+
+    }
+    int a,b;
+    a = atoi(s1.c_str());
+    b = atoi(s2.c_str());
+    cout << a << ' ' << b;
     return 0;
 }
