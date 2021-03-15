77bef2a46ae4f39cb1aac60bd60bba8e7116834e
zimka <zimkail2015@mail.ru>
2021-03-15 20:18:01 +0300

    Чекер Добавил чекер, который будет проверять на правильность написанное выражение. Однако, он ещё не может выводить причину и показывает все ошибки, а не первую.

--- clc.cpp
+++ clc.cpp
@@ -1,24 +1,89 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 
-int main()
+string s, s1, s2,mist;
+int fl = 0,n;
+
+void check ()
 {
-    string s, s1, s2;
-    char sign;
-    getline(cin,s);
-    int n = s.size();
-    int fl = 0;
+    int k = 0,zn = 0;
     for (int i = 0; i < n; i++)
     {
+        if (s[i] == ' ') k++;
+
+
+        if (s[i] != '+' && s[i] != '-' && s[i] != '/' &&
+             s[i] != '*' && s[i] != ' ' && s[i] != '0' &&
+              s[i] != '1' && s[i] != '2' && s[i] != '3' &&
+               s[i] != '4' && s[i] != '5' && s[i] != '6' &&
+                s[i] != '7' && s[i] != '8' && s[i] != '9') mist[i] = '^';
+
+
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') zn++;
+
+
+        if (s[i] != '+' && s[i] != '-' && s[i] != '/' && s[i] != '*' && s[i] != ' ' && k != 0 && zn == 0)
+        {
+            int j = i - 1;
+            while (s[j] == ' ')
+            {
+                mist[j] = '^';
+                j--;
+            }
+        }
+        if (s[i] != '+' && s[i] != '-' && s[i] != '/' && s[i] != '*' && s[i] != ' ' && zn > 1)
+        {
+            int j = i;
+            while (s[j] != '+' && s[j] != '-' && s[j] != '/' && s[j] != '*')
+            {
+                j--;
+                if (s[j] == '+' || s[j] == '-' || s[j] == '/' || s[j] == '*')
+                    mist[j] = '^';
+            }
+        }
+        if (s[i] != '+' && s[i] != '-' && s[i] != '/' && s[i] != '*' && s[i] != ' ' && (k == 0 || zn != 0))
+        {
+            k = 0;
+            zn = 0;
+        }
+    }
+
+}
+
+int main()
+{
+    char sign;
+    getline(cin,s);
+    mist.resize(s.size());
+    n = s.size();
+    check();
+    for (int i = 0; i < n; i++)
+    {
+        if (fl > 0) s2 += s[i];
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') fl++;
         if (fl == 0) s1 += s[i];
-        if (fl == 1) sign = s[i];
-        if (fl == 3) s2 += s[i];
-        if (s[i] == ' ' || s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') fl++;
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') sign = s[i];
 
     }
+    fl = 0;
+    for (int i = 0 ; i < n; i++)
+    {
+        if (mist[i] == '^')
+        {
+            fl = 1;
+            break;
+        }
+    }
+    if (fl == 1)
+    {
+        cout << mist;
+        return 0;
+    }
+
     int a,b;
     a = atoi(s1.c_str());
     b = atoi(s2.c_str());
