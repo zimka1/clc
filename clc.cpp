120ba533853e9bafe507e15615247e9f5df08849
zimka <zimkail2015@mail.ru>
2021-03-16 17:51:17 +0300

    Отрицательные числа Теперь можно производить вычисления с отрицательными числами.

--- clc.cpp
+++ clc.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 string s, s1, s2, mist, why = " ";
-int fl = 0,n;
+int fl = 0, n, fi = 0, mi = 0;
 
 void check ()
 {
@@ -28,7 +28,19 @@ void check ()
                 }
 
 
-        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') {zn++; pzn++;}
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*')
+        {
+
+            if (s[i] != '-' || (s[i] != '-' || (s[i+1] != '0' &&
+                                                s[i+1] != '1' && s[i+1] != '2' && s[i+1] != '3' &&
+                                                s[i+1] != '4' && s[i+1] != '5' && s[i+1] != '6' &&
+                                                s[i+1] != '7' && s[i+1] != '8' && s[i+1] != '9')))
+                                                    {
+                                                        zn++;
+                                                        pzn++;
+                                                    }
+
+        }
 
 
         if (pzn == 2 && zn <= 1)
@@ -77,19 +89,34 @@ void check ()
 
 int main()
 {
-    char sign;
+    char sign = ' ';
     getline(cin,s);
     mist.resize(s.size());
     n = s.size();
     check();
-    for (int i = 0; i < n; i++)
+    mi = 0;
+    fi = 0;
+    if (s[0] == '-' && (s[1] == '0' || s[1] == '1' ||
+                        s[1] == '2' || s[1] == '3' ||
+                        s[1] == '4' || s[1] == '5' ||
+                        s[1] == '6' || s[1] == '7' ||
+                        s[1] == '8' || s[1] == '9'))
+                        {
+                        fi = 1;
+                        }
+
+    for (int i = fi; i < n; i++)
     {
-        if (fl > 0) s2 += s[i];
+//        cout << s[i];
+        if (sign != ' ' && s[i] != '-') s2 += s[i];
         if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') fl++;
         if (fl == 0) s1 += s[i];
+        if (s[i] == '-' && sign != ' ') mi = 1;
+        else
         if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') sign = s[i];
 
     }
+
     if (why != " ")
     {
         cout << mist << endl;
@@ -100,6 +127,8 @@ int main()
     int a,b;
     a = atoi(s1.c_str());
     b = atoi(s2.c_str());
+    if (fi == 1) a *= -1;
+    if (mi == 1) b *= -1;
     if (sign == '+') cout << a + b;
     if (sign == '*') cout << a * b;
     if (sign == '-') cout << a - b;
